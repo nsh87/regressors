87b0f692fcaf62c44cee78f63fe479ab27c1e338
Nikhil Haas <nikhil@nikhilhaas.com>
2015-11-22 16:00:52 -0800

    Create LinearRegression class that provides a summary of coeffs - Inherits from scikit-learn's LinearRegression class. Overrides fit()   method to allow passing an extra parameter 'xlabels', which is used   to label the coefficients in a new method summary(). - summary() provides output of labeled coefficients. Should be added   later to include p-values, R squared, adjusted R squared, etc. - Includes tests for the fit() method override and summary() method.

--- regressors/regressors.py
+++ regressors/regressors.py
@@ -1,1 +1,70 @@
 # -*- coding: utf-8 -*-
+
+"""This module contains core classes for regression models."""
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from sklearn import linear_model
+import numpy as np
+import pandas as pd
+
+
+class LinearRegression(linear_model.LinearRegression):
+
+    def __init__(self, fit_intercept=True, normalize=False, copy_X=True,
+                 n_jobs=1):
+        self.fit_intercept = fit_intercept
+        self.normalize = normalize
+        self.copy_X = copy_X
+        self.n_jobs = n_jobs
+        self.xlabels = None
+
+    def fit(self, *args, **kwargs):
+        # Try to get xlabels from *args or **kwargs
+        xlabels = kwargs.get('xlabels')  # Get xlabels or None if not in kwargs
+        if xlabels is None:
+            try:
+                xlabels = args[2]
+                args = list(args)
+                args.pop()  # Remove xlabels from args
+            except IndexError:
+                # No xlabels in args, either
+                pass
+        else:
+            del kwargs['xlabels']  # Remove xlabels from kwargs
+        # Get X matrix
+        try:
+            X = kwargs['X']
+        except KeyError:
+            X = args[0]
+        # Make xlabels if none were passed
+        ncols = X.shape[1]
+        if xlabels is None:
+            xlabels = np.array(
+                ['x{0}'.format(i) for i in range(ncols)], dtype='str')
+        elif isinstance(xlabels, (tuple, list)):
+            xlabels = np.array(xlabels, dtype='str')
+        # Make sure dims of xlabels matches dims of X
+        if len(xlabels) != ncols:
+            raise IndexError(
+                "Dimension of xlabels {0} does not match "
+                "X {1}.".format(xlabels.shape, X.shape))
+        self.xlabels = xlabels
+        # Call parent .fit() method
+        super(LinearRegression, self).fit(*args, **kwargs)
+
+
+    def summary(self, xlabels=None):
+        """Return summary results of fitted model.
+
+        Parameters
+        ----------
+        xlabels : list, tuple, numpy.ndarray
+            Optional labels for the coefficients, corresponding to columns of X.
+        """
+        coeffs = np.concatenate((np.array([self.intercept_]), self.coef_))
+        labels = np.concatenate((np.array(['_intercept']), self.xlabels))
+        summary = {'coef': pd.Series(coeffs, index=labels)}
+        df = pd.DataFrame(summary)
+        return df
--- tests/test_regressors.py
+++ tests/test_regressors.py
@@ -3,26 +3,97 @@
 
 """
 test_regressors
-----------------------------------
+---------------
 
-Tests for `regressors` module.
+Tests for the `regressors` module.
 """
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
 
-import unittest
+import unittest2 as unittest
+from sklearn import datasets
+import pandas as pd
+import numpy as np
 
 from regressors import regressors
 
 
-class TestRegressors(unittest.TestCase):
+class TestLinearRegression(unittest.TestCase):
 
     def setUp(self):
-        pass
+        boston = datasets.load_boston()
+        which_betas = np.ones(13, dtype=bool)
+        which_betas[3] = False  # Eliminate dummy variable
+        self.X = boston.data[:, which_betas]
+        self.y = boston.target
 
     def tearDown(self):
         pass
 
-    def test_000_something(self):
-        pass
+    def test_LinearRegression_fit_with_no_xlabels(self):
+        ols = regressors.LinearRegression()
+        try:
+            ols.fit(self.X, self.y)
+        except Exception as e:
+            self.fail("Fitting with no xlabels raised unexpected "
+                      "exception: {0}".format(e))
+
+    def test_LinearRegression_fit_with_xlabels_as_args(self):
+        ols = regressors.LinearRegression()
+        labels = ['LABEL{0}'.format(i) for i in range(self.X.shape[1])]
+        try:
+            ols.fit(self.X, self.y, labels)
+        except Exception as e:
+            self.fail("Fitting with xlabels as *args raised unexpected "
+                      "exception: {0}".format(e))
+
+    def test_LinearRegression_fit_with_xlabels_as_kwargs(self):
+        ols = regressors.LinearRegression()
+        labels = ['LABEL{0}'.format(i) for i in range(self.X.shape[1])]
+        try:
+            ols.fit(self.X, y=self.y, xlabels=labels)
+        except Exception as e:
+            self.fail("Fitting with xlabels as **kwargs raised unexpected "
+                      "exception: {0}".format(e))
+
+    def test_LinearRegression_fit_with_xlabels_mixed_kwarg(self):
+        ols = regressors.LinearRegression()
+        labels = ['LABEL{0}'.format(i) for i in range(self.X.shape[1])]
+        try:
+            ols.fit(self.X, self.y, xlabels=labels)
+        except Exception as e:
+            self.fail("Fitting with xlabels as **kwargs with y also as "
+                      "**kwargs raised unexpected exception: {0}".format(e))
+
+    def test_LinearRegression_fit_with_xlabels_all_kwargs(self):
+        ols = regressors.LinearRegression()
+        labels = ['LABEL{0}'.format(i) for i in range(self.X.shape[1])]
+        try:
+            ols.fit(X=self.X, y=self.y, xlabels=labels)
+        except Exception as e:
+            self.fail("Fitting with xlabels with all parameters as "
+                      "**kwargs raised unexpected exception: {0}".format(e))
+
+    def test_LinearRegression_xlabel_dimensions_error_checking(self):
+        ols = regressors.LinearRegression()
+        with self.assertRaises(IndexError):
+            ols.fit(self.X, self.y, xlabels=['LABEL1', 'LABEL2'])
+
+    def test_LinearRegression_summary(self):
+        ols = regressors.LinearRegression()
+        labels = ['LABEL{0}'.format(i) for i in range(self.X.shape[1])]
+        ols.fit(self.X, self.y, labels)
+        summary = ols.summary()
+        self.assertIsInstance(summary, pd.core.frame.DataFrame)
+        try:
+            str(summary)
+        except Exception as e:
+            self.fail("str(summary) raised "
+                      "exception unexpectedly: {0}".format(e))
+
+
 
 
 if __name__ == '__main__':
