e321210fd177c49f7399fdc7aa8c4b9959c80d01
Nikhil Haas <nikhil@nikhilhaas.com>
2015-11-26 15:05:25 -0800

    Update tests to use global X and Y data - You were having to load and create training data repeatedly   in setUp() methods in the test classes.

--- tests/test_regressors.py
+++ tests/test_regressors.py
@@ -18,16 +18,19 @@ import pandas as pd
 import numpy as np
 
 from regressors import regressors
+from regressors import stats
+
+boston = datasets.load_boston()
+which_betas = np.ones(13, dtype=bool)
+which_betas[3] = False  # Eliminate dummy variable
+X = boston.data[:, which_betas]
+y = boston.target
 
 
 class TestLinearRegression(unittest.TestCase):
 
     def setUp(self):
-        boston = datasets.load_boston()
-        which_betas = np.ones(13, dtype=bool)
-        which_betas[3] = False  # Eliminate dummy variable
-        self.X = boston.data[:, which_betas]
-        self.y = boston.target
+        pass
 
     def tearDown(self):
         pass
@@ -35,71 +38,71 @@ class TestLinearRegression(unittest.TestCase):
     def test_LinearRegression_fit_with_no_xlabels(self):
         ols = regressors.LinearRegression()
         try:
-            ols.fit(self.X, self.y)
+            ols.fit(X, y)
         except Exception as e:
             self.fail("Fitting with no xlabels raised unexpected "
                       "exception: {0}".format(e))
 
     def test_LinearRegression_fit_with_xlabels_as_args(self):
         ols = regressors.LinearRegression()
-        labels = ['LABEL{0}'.format(i) for i in range(self.X.shape[1])]
+        labels = ['LABEL{0}'.format(i) for i in range(X.shape[1])]
         try:
-            ols.fit(self.X, self.y, labels)
+            ols.fit(X, y, labels)
         except Exception as e:
             self.fail("Fitting with xlabels as *args raised unexpected "
                       "exception: {0}".format(e))
 
     def test_LinearRegression_fit_with_xlabels_as_kwargs(self):
         ols = regressors.LinearRegression()
-        labels = ['LABEL{0}'.format(i) for i in range(self.X.shape[1])]
+        labels = ['LABEL{0}'.format(i) for i in range(X.shape[1])]
         try:
-            ols.fit(self.X, y=self.y, xlabels=labels)
+            ols.fit(X, y=y, xlabels=labels)
         except Exception as e:
             self.fail("Fitting with xlabels as **kwargs raised unexpected "
                       "exception: {0}".format(e))
 
     def test_LinearRegression_fit_with_xlabels_mixed_kwarg(self):
         ols = regressors.LinearRegression()
-        labels = ['LABEL{0}'.format(i) for i in range(self.X.shape[1])]
+        labels = ['LABEL{0}'.format(i) for i in range(X.shape[1])]
         try:
-            ols.fit(self.X, self.y, xlabels=labels)
+            ols.fit(X, y, xlabels=labels)
         except Exception as e:
             self.fail("Fitting with xlabels as **kwargs with y also as "
                       "**kwargs raised unexpected exception: {0}".format(e))
 
     def test_LinearRegression_fit_with_xlabels_all_kwargs(self):
         ols = regressors.LinearRegression()
-        labels = ['LABEL{0}'.format(i) for i in range(self.X.shape[1])]
+        labels = ['LABEL{0}'.format(i) for i in range(X.shape[1])]
         try:
-            ols.fit(X=self.X, y=self.y, xlabels=labels)
+            ols.fit(X=X, y=y, xlabels=labels)
         except Exception as e:
             self.fail("Fitting with xlabels with all parameters as "
                       "**kwargs raised unexpected exception: {0}".format(e))
 
     def test_LinearRegression_fit_with_xlabels_out_of_position_kwargs(self):
         ols = regressors.LinearRegression()
-        labels = ['LABEL{0}'.format(i) for i in range(self.X.shape[1])]
+        labels = ['LABEL{0}'.format(i) for i in range(X.shape[1])]
         try:
-            ols.fit(X=self.X, xlabels=labels, y=self.y)
+            ols.fit(X=X, xlabels=labels, y=y)
         except Exception as e:
             self.fail("Fitting with xlabels with all parameters as "
                       "**kwargs raised unexpected exception: {0}".format(e))
 
     def test_LinearRegression_fit_with_xlabels_args_out_of_pos_args_fails(self):
         ols = regressors.LinearRegression()
-        labels = ['LABEL{0}'.format(i) for i in range(self.X.shape[1])]
+        labels = ['LABEL{0}'.format(i) for i in range(X.shape[1])]
         with self.assertRaises(AssertionError):
-            ols.fit(self.X, labels, self.y)
+            ols.fit(X, labels, y)
 
     def test_LinearRegression_xlabel_dimensions_error_checking(self):
         ols = regressors.LinearRegression()
         with self.assertRaises(AssertionError):
-            ols.fit(self.X, self.y, xlabels=['LABEL1', 'LABEL2'])
+            ols.fit(X, y, xlabels=['LABEL1', 'LABEL2'])
 
     def test_LinearRegression_summary(self):
         ols = regressors.LinearRegression()
-        labels = ['LABEL{0}'.format(i) for i in range(self.X.shape[1])]
-        ols.fit(self.X, self.y, labels)
+        labels = ['LABEL{0}'.format(i) for i in range(X.shape[1])]
+        ols.fit(X, y, labels)
         summary = ols.summary()
         self.assertIsInstance(summary, pd.core.frame.DataFrame)
         try:
