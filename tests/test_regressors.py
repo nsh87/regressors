df06d5cceb319990f72ba3353b324f917c11eabd
Nikhil Haas <nikhil@nikhilhaas.com>
2015-12-07 17:04:46 -0800

    Add test to make sure summary function doesn't error - Only tests a few regression models. We need to add more to the list   later.

--- tests/test_regressors.py
+++ tests/test_regressors.py
@@ -238,6 +238,17 @@ class TestStats(unittest.TestCase):
                           "supported linear models failed "
                           "unexpectedly: {0}".format(e))
 
+    def test_error_not_raised_by_summary_function(self):
+        for classifier in regressors.supported_linear_models:
+            clf = classifier()
+            clf.fit(X, y)
+            try:
+                stats.f_stat(clf, X, y).shape
+            except Exception as e:
+                self.fail("Testing summary function for "
+                          "supported linear models failed "
+                          "unexpectedly: {0}".format(e))
+
 if __name__ == '__main__':
     import sys
     sys.exit(unittest.main())
